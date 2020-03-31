58c10272919559c33456bbe5e107fdc68fae0bca
Bohdan Borkivskyi <bohdan.borkivskyi@quintagroup.org>
2020-03-31 17:04:14 +0300

    fix not working tests

--- src/openprocurement/tender/location/tests/base.py
+++ src/openprocurement/tender/location/tests/base.py
@@ -1,8 +1,14 @@
 import json
 import os
 
+from openprocurement.tender.core.tests.base import BaseWebTest
+
 here = os.path.dirname(os.path.abspath(__file__))
 
 
 with open(os.path.join(here, "data/tender_data.json")) as _in:
     test_tender_data = json.load(_in)
+
+
+class BaseApiWebTest(BaseWebTest):
+    relative_to = os.path.dirname(__file__)
--- src/openprocurement/tender/location/tests/tender_blanks.py
+++ src/openprocurement/tender/location/tests/tender_blanks.py
@@ -3,7 +3,5 @@ from copy import deepcopy
 
 def create_tender(self):
     data = deepcopy(self.initial_data)
-    #data.update({"status": "draft", "value": {"amount": 179511.28, "currency": "UAH", "valueAddedTaxIncluded": True}})
     response = self.app.post_json("/tenders", {"data": data})
     self.assertEqual((response.status, response.content_type), ("201 Created", "application/json"))
-    self.assertNotIn("value", response.json["data"])
